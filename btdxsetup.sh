579f377b157de5e7abeb242a815803c295a96e3f
chromster <32965831+chromster@users.noreply.github.com>
2017-10-28 19:43:00 +0200

    Update btdxsetup.sh

--- btdxsetup.sh
+++ btdxsetup.sh
@@ -57,30 +57,30 @@ cp bitcloud-tx /usr/local/bin
 cd
 
 echo '*** Done 3/5 ***'
-sleep 2
-echo '*** Step 4/5 ***'
-echo '*** Configure bitcloud.conf and download and import bootstrap file ***'
-sleep 2
-
-bitcloudd
-sleep 3
-
-echo -n "Please Enter a STRONG Password or copy & paste the password generated for you above and Hit [ENTER]: "
-read usrpas
-echo -n "Please Enter your masternode genkey respond and Hit [ENTER]: "
-read mngenkey
-
-echo -e "rpcuser=btdxdmasternodeservice2387645 \nrpcpassword=$usrpas \nrpcallowip=127.0.0.1 \nserver=1 \nlisten=1 \ndaemon=1 \nlogtimestamps=1 \nmasternode=1 \npromode=1 \nmasternodeprivkey=$mngenkey \n" > ~/.bitcloud/bitcloud.conf
-
-echo '*** Done 4/5 ***'
-sleep 2
-echo '*** Step 5/5 ***'
-echo '*** Last Server Start also Wallet Sync ***'
-echo 'After 1 minute you will see the 'getinfo' output from the RPC Server...'
-bitcloudd
-sleep 60
-bitcloud-cli getinfo
-sleep 2
-echo 'Have fun with your Masternode !'
-sleep 2
-echo '*** Done 5/5 ***'
+#sleep 2
+#echo '*** Step 4/5 ***'
+#echo '*** Configure bitcloud.conf and download and import bootstrap file ***'
+#sleep 2
+#
+#bitcloudd
+#sleep 3
+#
+#echo -n "Please Enter a STRONG Password or copy & paste the password generated for you above and Hit [ENTER]: "
+#read usrpas
+#echo -n "Please Enter your masternode genkey respond and Hit [ENTER]: "
+#read mngenkey
+#
+#echo -e "rpcuser=btdxdmasternodeservice2387645 \nrpcpassword=$usrpas \nrpcallowip=127.0.0.1 \nserver=1 \nlisten=1 \ndaemon=1 \nlogtimestamps=1 \nmasternode=1 \npromode=1 \nmasternodeprivkey=$mngenkey \n" > ~/.bitcloud/bitcloud.conf
+#
+#echo '*** Done 4/5 ***'
+#sleep 2
+#echo '*** Step 5/5 ***'
+#echo '*** Last Server Start also Wallet Sync ***'
+#echo 'After 1 minute you will see the 'getinfo' output from the RPC Server...'
+#bitcloudd
+#sleep 60
+#itcloud-cli getinfo
+#sleep 2
+#echo 'Have fun with your Masternode !'
+#sleep 2
+#echo '*** Done 5/5 ***'
